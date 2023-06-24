d273e016af917d9db4134425be5f65d9227eb320
SoulAndBone <soulandbone@gmail.com>
2023-06-24 16:37:04 +0300

    added search bar, and some minor changes to the Header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -12,33 +12,95 @@ class _HeaderState extends State<Header> {
     Size size = MediaQuery.of(context).size;
     return SliverList(
         delegate: SliverChildListDelegate([
-      Column(
+      Stack(
+        // has one column inside
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            height: size.height / 5,
-            decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 2,
+          Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                height: size.height / 5,
+                decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                      )
+                    ],
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(45),
+                    )),
+                child: Column(children: [
+                  const Gap(20),
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Colors.white70,
+                        child: CircleAvatar(
+                            radius: 30,
+                            backgroundImage:
+                                AssetImage('assets/images/dog.jpg')),
+                      ),
+                      const Gap(5),
+                      Column(
+                        // this is the column for Good Boi and "he is a good Boy"
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Good Boi',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.circular(20)),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 4),
+                            child: const Text(
+                              'He is a good boy',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      ),
+                      const Spacer(),
+                      const Text(
+                        '154 \$CAN',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
                   )
-                ],
-                color: Colors.teal,
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(45),
-                )),
-            child: const Column(children: [
-              Gap(20),
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 35,
-                    backgroundColor: Colors.white70,
-                  )
-                ],
-              )
-            ]),
-          )
+                ]),
+              ),
+              const Gap(15)
+            ],
+          ),
+          Positioned(
+              bottom: 0,
+              child: Container(
+                height: 50,
+                width: size.width,
+                child: Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  elevation: 3,
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'What does your belly want to eat ?',
+                          labelStyle: TextStyle(
+                              fontSize: 13, fontWeight: FontWeight.w200),
+                          suffixIcon: Icon(Icons.search),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(left: 20))),
+                ),
+              )),
         ],
       )
     ]));
